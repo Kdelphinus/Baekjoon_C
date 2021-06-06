227d56d70ca2990b61044b79afc1c06f8708fbb6
Kdelphinus <delphinus@khu.ac.kr>
2021-06-06 16:55:09 +0900

    14-7, 8 해결

--- 14. 백트래킹/07. 연산자 끼워넣기.c
+++ 14. 백트래킹/07. 연산자 끼워넣기.c
@@ -1,37 +1,13 @@
 #include <stdio.h>
 
 int num, sum, min = 1000000000, max = -1000000000;
-int number[101];   // 숫자를 저장할 배열
-int operator[101]; // 연산자를 저장할 배열
-int result[101];   // 연산자의 조합을 저장할 배열
-int temp[4];       // 연산자의 개수를 저장할 배열
-int check[4];      // 연산자 조합 중복을 방지하기 위한 배열
+int number[101]; // 숫자를 저장할 배열
+int operator[4]; // 연산자의 개수를 저장할 배열
 
 int DFS(int l)
 {
     if (l == num - 1) // 연산자 조합을 다 했다면
     {
-        sum = number[0]; // 첫 숫자로 시작
-        for (int i = 1; i < num; i++)
-        {
-            if (result[i - 1] == 0) // +
-                sum += number[i];
-            else if (result[i - 1] == 1) // -
-                sum -= number[i];
-            else if (result[i - 1] == 2) // x
-                sum *= number[i];
-            else if (result[i - 1] == 3) // /
-            {
-                if (sum < 0)
-                { // 음수를 양수로 나눌 때, 음수를 양수로 바꾸고 나눈 뒤, 몫을 음수로 바꿈
-                    sum = -sum;
-                    sum /= number[i];
-                    sum = -sum;
-                }
-                else
-                    sum /= number[i];
-            }
-        }
         if (min > sum) // 최솟값
             min = sum;
         if (max < sum) // 최댓값
@@ -39,14 +15,25 @@ int DFS(int l)
     }
     else
     {
-        for (int i = 0; i < num - 1; i++)
+        for (int i = 0; i < 4; i++)
         {
-            if (check[operator[i]] < temp[operator[i]]) // 받은 개수만큼만 사용
+            if (l == 0) // 받은 개수만큼만 사용
+                sum = number[l];
+            if (operator[i]> 0) // 연산자가 아직 남아있으면
             {
-                check[operator[i]]++;
-                result[l] = operator[i];
-                DFS(l + 1);
-                check[operator[i]]--;
+                int tmp = sum; // 원본을 저장하고
+                if (i == 0)    // +
+                    sum += number[l + 1];
+                else if (i == 1) // -
+                    sum -= number[l + 1];
+                else if (i == 2) // *
+                    sum *= number[l + 1];
+                else // /
+                    sum /= number[l + 1];
+                operator[i]--; // 사용한 연산자의 개수를 차감
+                DFS(l + 1);    // 다음 위치 연산자로 이동
+                operator[i]++; // 돌아온 연산자 추가
+                sum = tmp;     // 연산 전 결과값으로 변경
             }
         }
     }
@@ -60,16 +47,8 @@ int main()
         scanf("%d", &number[i]);
 
     for (int i = 0; i < 4; i++) // 연산자의 개수들을 받음
-        scanf("%d", &temp[i]);
+        scanf("%d", &operator[i]);
 
-    int oper, j = 0;
-    for (int i = 0; i < 4; i++) // +: 0, -: 1, *: 2, /: 3
-    {
-        oper = i;
-
-        for (int k = 0; k < temp[i]; k++) // 연산자 개수만큼 추가
-            operator[j++] = oper;
-    }
     DFS(0);
 
     printf("%d\n", max);
