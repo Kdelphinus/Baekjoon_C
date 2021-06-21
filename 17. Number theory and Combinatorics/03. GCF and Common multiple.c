00578ed4e6c94fba5d71b9568e838cba08a397c9
Kdelphinus <delphinus@khu.ac.kr>
2021-06-21 16:14:26 +0900

    17-1, 2, 3 해결

---
+++ 17. Number theory and Combinatorics/03. GCF and Common multiple.c
@@ -0,0 +1,62 @@
+/**
+ * @file 03. GCF and Common multiple.c
+ * @author Kdelphinus (delphinus@khu.ac.kr)
+ * @brief 유클리드 호제법과 최소 공배수와 최대 공약수 사이의 관계를 이용
+ * @date 2021-06-21 16:13:36
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+
+int GCF(int a, int b) // 최대공약수를 구하는 함수
+{
+    if (a == b)
+        return a;
+
+    int a_tmp, b_tmp;
+    a_tmp = a;
+    b_tmp = b;
+
+    while (a_tmp > 0) // 유클리드 호제법 이용
+    {
+        int remain;
+        remain = b_tmp % a_tmp;
+        b_tmp = a_tmp;
+        a_tmp = remain;
+    }
+
+    return b_tmp;
+}
+
+int common_multiple(int a, int b) // 최소 공배수를 구하는 함수
+{
+    if (a == b)
+        return a;
+
+    int min, tmp;
+    min = GCF(a, b);
+    tmp = a * b;
+    if (tmp < 0)
+        tmp *= -1;
+
+    return tmp / min; // 최소 공배수와 최대 공약수 사이 관계를 이용
+}
+
+int main()
+{
+    int num1, num2, min;
+    scanf("%d %d", &num1, &num2);
+    if (num2 < num1)
+    {
+        int temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    printf("%d\n", GCF(num1, num2));
+    printf("%d", common_multiple(num1, num2));
+
+    return 0;
+}
