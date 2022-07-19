cd5fb1b002b6e36624b14ffc82a4948f4a8a43bc
Kdelphinus <kmj951015@gmail.com>
2022-07-19 23:15:53 +0900

    solve

--- baekjoon/12_set_and_map/04_number_card_2.c
+++ baekjoon/12_set_and_map/04_number_card_2.c
@@ -2,38 +2,49 @@
 #include <stdlib.h>
 
 int	cards[500001];
-int	check[500001];
 
 int	compare(const void *a, const void *b)
 {
 	return (*(int *)a - *(int *)b);
 }
 
-int	binary_search(int num, int cnt, int start, int end)
+int	binary_upper_search(int num, int start, int end)
 {
-	int	mid, result;
+	int	mid;
 
-	if (start > end)
-		return cnt;
-	
-	mid = (start + end) / 2;
-
-	if (cards[mid] > num)
-		return (binary_search(num, cnt, start, mid - 1));
-	else if (cards[mid] < num)
-		return (binary_search(num, cnt, mid + 1, end));
-	else
+	while (start <= end)
 	{
-		result = binary_search(num, cnt, start, mid - 1);
-		result += binary_search(num, cnt, mid + 1, end);
-		result++;
+		mid = (start + end) / 2;
+		if (cards[mid] == num && (mid - 1 < 0 || cards[mid - 1] < num))
+			return (mid);
+		else if (cards[mid] < num)
+			start = mid + 1;
+		else
+			end = mid - 1;
 	}
-	return (result);
+	return (-1);
+}
+
+int	binary_lower_search(int num, int n, int start, int end)
+{
+	int	mid;
+
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (cards[mid] == num && (mid + 1 >= n || cards[mid + 1] > num))
+			return (mid);
+		else if (cards[mid] > num)
+			end = mid - 1;
+		else
+			start = mid + 1;
+	}
+	return (-1);
 }
 
 int	main(void)
 {
-	int	n, m;
+	int	n, m, check, start, end;
 
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
@@ -43,10 +54,16 @@ int	main(void)
 
 	scanf("%d", &m);
 	for (int i = 0; i < m; i++)
-		scanf("%d", &check[i]);
-
-	for (int i = 0; i < m; i++)
-		printf("%d ", binary_search(check[i], 0, 0, n - 1));
+	{
+		scanf("%d", &check);
+		end = binary_lower_search(check, n, 0, n - 1);
+		start = binary_upper_search(check, 0, n - 1);
+	
+		if (end > -1 && start > -1)
+			printf("%d ", end - start + 1);
+		else
+			printf("0 ");
+	}
 	printf("\n");
 	return (0);
 }
